0f42e643495bd8ae4c499546fd39d39f2c8631dd
Caleb Phillips <caleb.phillips@nrel.gov>
2020-08-26 17:02:11 -0600

    Windrose working and looking correct!

--- src/App.js
+++ src/App.js
@@ -4,34 +4,46 @@ import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import Axios from 'axios';
 import { trackPromise } from 'react-promise-tracker';
 import Plotly from 'plotly.js'
-//import _ from 'lodash'
+import _ from 'lodash'
 import L from 'leaflet'
 import LoadingSpinnerComponent from './LoadingSpinnerComponent'
 import SimpleModal from './SimpleModal'
 
 function drawPopup(data,marker,popup){
-  console.log(marker)
-  console.log(popup)
   var plot = [{
-    r: [77.5, 72.5, 70.0, 45.0, 22.5, 42.5, 40.0, 62.5],
+    r: _.zipWith(_.zipWith(data[">20 m/s"],data["10-20 m/s"],data["5-10 m/s"],data["<5 m/s"],function(a,b,c,d) {
+        return a + b + c + d;
+      }),data["Any"],function(a,b) {
+      return a * (b / 100.0);
+      }),
     theta: ["North", "N-E", "East", "S-E", "South", "S-W", "West", "N-W"],
-    name: "11-14 m/s",
+    name: "> 20 m/s",
     marker: {color: "rgb(106,81,163)"},
     type: "barpolar"
   }, {
-    r: [57.5, 50.0, 45.0, 35.0, 20.0, 22.5, 37.5, 55.0],
+    r: _.zipWith(_.zipWith(data["10-20 m/s"],data["5-10 m/s"],data["<5 m/s"],function(a,b,c) {
+      return a + b + c;
+    }),data["Any"],function(a,b) {
+      return a * (b / 100.0);
+    }),
     theta: ["North", "N-E", "East", "S-E", "South", "S-W", "West", "N-W"],
-    name: "8-11 m/s",
+    name: "10-20 m/s",
     marker: {color: "rgb(158,154,200)"},
     type: "barpolar"
   }, {
-    r: [40.0, 30.0, 30.0, 35.0, 7.5, 7.5, 32.5, 40.0],
+    r: _.zipWith(_.zipWith(data["5-10 m/s"],data["<5 m/s"],function(a,b) {
+      return a + b;
+    }),data["Any"],function(a,b) {
+      return a * (b / 100.0);
+    }),
     theta: ["North", "N-E", "East", "S-E", "South", "S-W", "West", "N-W"],
-    name: "5-8 m/s",
+    name: "5-10 m/s",
     marker: {color: "rgb(203,201,226)"},
     type: "barpolar"
   }, {
-    r: [20.0, 7.5, 15.0, 22.5, 2.5, 2.5, 12.5, 22.5],
+    r: _.zipWith(data["<5 m/s"],data["Any"],function(a,b){
+      return a * (b / 100.0);
+    }),
     theta: ["North", "N-E", "East", "S-E", "South", "S-W", "West", "N-W"],
     name: "< 5 m/s",
     marker: {color: "rgb(242,240,247)"},
@@ -49,19 +61,6 @@ function drawPopup(data,marker,popup){
     }
   }
   Plotly.newPlot("plotly", plot, layout)
-/*  let plot = [{
-    x: _.values(data.timestamp),
-    y: _.values(data.windspeed),
-    type: 'scatter'
-  }];
-  var layout = {
-    autosize: false,
-    width: 800,
-    height: 600,
-    staticPlot: true
-  };
-  console.log(plot)
-  Plotly.newPlot( div, plot, layout);*/
 }
 
 type AppState = {
@@ -97,8 +96,8 @@ export class App extends Component<{},AppState> {
     this.setState({lat: latlng.lat, lng: latlng.lng});
     this.mapRef.current.leafletElement.panTo(latlng);
     trackPromise(
-        Axios.get('http://localhost:8080/v1/timeseries/windspeed?height=67.00m&lat='+this.state.lat.toString()+'&lon='+this.state.lng.toString()+
-            '&start_date=20070302&stop_date=20070402&vertical_interpolation=nearest&spatial_interpolation=idw').then(function(response){
+        Axios.get('http://localhost:8080/v1/windrose?height=67.00m&lat='+this.state.lat.toString()+'&lon='+this.state.lng.toString()+
+            '&start_date=20070101&stop_date=20070301&vertical_interpolation=nearest&spatial_interpolation=idw').then(function(response){
           var popup = L.popup().setContent('You clicked the map at ' + latlng.toString());
           marker.bindPopup(popup).openPopup();
           drawPopup(response.data,marker,popup)
